543868716cffccae5293898822fedf869a2e4f4b
christ <christikonga83@gmail.com>
2022-08-17 13:12:32 +0200

    Django backend views and urls

---
+++ Backend/Backend/data/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=68,min_length=8,write_only=True)
+    first_name = serializers.CharField(max_length=120)
+    last_name = serializers.CharField(max_length=120)
+    
+    class Meta:
+        model = User
+        fields = ['last_name','first_name','email','password']
+
+    
+    def validate(self,attrs):
+        email = attrs.get('email','')
+        first_name = attrs.get('first_name','')
+        last_name = attrs.get('last_name','')
+
+        if not first_name.isalnum() and not last_name.isalnum():
+            serializers.ValidationError("first_name and last_name should only contain alpha numeric character")
+                
+        return attrs
+
+    def create(self,validated_data):
+         return User.objects.create_user(**validated_data)
--- Backend/Backend/data/views.py
+++ Backend/Backend/data/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import generics,status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .utils import Util
+from .models import User
+from .serializers import *
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+class RegisterView(generics.GenericAPIView):
+    serilazer_class = RegisterSerializer
+    def post(self,request):
+        user = request.data
+        serializer = self.serilazer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        
+        user = User.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user).access_token
 
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('verifyEmail')
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        email_body = "Hi"+" "+user.first_name+" use link to verify your mail \n"+absurl
+        data = {'email_body':email_body,'to_email':user.email,'email_subject':'verify your mail'}
+       
+        Util.send_mail(data)
+        return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass 
 # Create your views here.
