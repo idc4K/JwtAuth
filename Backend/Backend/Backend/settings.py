687e415a5091c8e1866b158bdd05b52d842085f8
christ <christikonga83@gmail.com>
2022-08-17 13:28:05 +0200

    Django backend views and urls

--- Backend/Backend/Backend/settings.py
+++ Backend/Backend/Backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'data'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'data.User'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_USE_TLS =  True
+EMAIL_PORT =  587
+EMAIL_HOST_USER = '*****************************'
+EMAIL_HOST_PASSWORD = '***********************'
+EMAIL_USE_TLS: True
