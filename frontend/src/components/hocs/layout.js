04879d44b5eeb90778ef4a1b80a5302c090c263c
christ <christikonga83@gmail.com>
2022-08-18 11:09:56 +0200

    frontend pages

---
+++ frontend/src/components/hocs/layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Navbar from '../NavBar/Navbar';
+
+function layout() {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default layout
