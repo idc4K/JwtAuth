eb20cc8d782a0d76c1c704a7233045ef71640b00
christ <christikonga83@gmail.com>
2022-08-18 12:14:31 +0200

    frontend Login

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react';
+import {Link,Redirect } from 'react-router-dom';
+import { connect } from 'redux';
 
 function Login() {
+  const[formData,setData] = useState({
+    email:'',
+    password:''
+  });
+  const {email,password } = formData;
+
+  const handleChange = e =>{
+    setData({
+        ...formData,
+        [e.target.name] : e.target.value
+    })
+
+  }
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+  }
   return (
     <div>Login</div>
   )
 }
 
-export default Login
+export default connect()(Login);
