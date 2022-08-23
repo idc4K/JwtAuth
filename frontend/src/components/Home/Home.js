c104484c092242d3c47d3c51f7ace3c2b49b5f95
christ <christikonga83@gmail.com>
2022-08-23 12:27:30 +0200

    navbar and home

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,10 +1,17 @@
-import React from 'react';
-
+import React,{useNavigate} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <div className="container">
+      <h1>HOME</h1>
+       <div className="jumbotron mt-5">
+         <h1 className='display-4'>welcome To Auth Systtem</h1>
+         <p className='lead'>THis is an incredible authentication system with production lele features</p>
+         <hr className='my-4'/>
+         <p>Click the log In button</p>
+         <button>Login</button>
+       </div>
+  </div>
   )
 }
 
--- frontend/src/components/NavBar/Navbar.js
+++ frontend/src/components/NavBar/Navbar.js
@@ -1,10 +1,32 @@
-import React from 'react'
-
+import React from 'react';
+import {Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Navbar() {
   return (
-    <div>
-        <h1 style={{color: "black"}}>Navbar</h1>
+    <nav className="navbar navbar-expand-lg" style={{backgroundColor:"black"}}>
+    <div className="container-fluid">
+        <Link className="navbar-brand" to="/" style={{color:"white"}}>Navbar</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/" style={{color:"white"}}>Header</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/header1" style={{color:"white"}}>Header1</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/header2" style={{color:"white"}}>Header2</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link disabled" to="/"style={{color:"grey"}}>Disabled</Link>
+            </li>
+        </ul>
+        </div>
     </div>
+</nav>
   )
 }
 
