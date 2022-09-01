280fde2c727cbc1e5b5faad84f3cb747236909fa
christ <christikonga83@gmail.com>
2022-09-01 12:48:31 +0200

    update reducer

--- Backend/Backend/Backend/urls.py
+++ Backend/Backend/Backend/urls.py
@@ -20,7 +20,7 @@ from django.urls import path,include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework_simplejwt.views import TokenVerifyView
+
 
 ...
 
@@ -39,7 +39,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('data.urls')),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- Backend/Backend/data/urls.py
+++ Backend/Backend/data/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from .views import *
-
+from rest_framework_simplejwt.views import TokenVerifyView
 
 
 
@@ -9,6 +9,5 @@ urlpatterns = [
     path('Register/', RegisterView.as_view(), name="register"),
     path('Verify/', VerifyEmail.as_view(), name="verifyEmail"),
     path('Login/', Login.as_view(), name="login"),
-    
-   
+      path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
