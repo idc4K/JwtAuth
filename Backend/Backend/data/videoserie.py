daa150937ae510b5acae2ebb76b471b0b417e117
christ <christikonga83@gmail.com>
2022-09-05 21:17:57 +0200

    database add video file

--- Backend/Backend/data/videoserie.py
+++ Backend/Backend/data/videoserie.py
@@ -5,7 +5,7 @@ from .serie import *
 class video_serie(models.Model):
     id =  models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
-    video_serie = models.FileField(upload_to="videos_serie/")
+    video_serie = models.FileField(upload_to="videos_serie/",null=True)
     deletevideo = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
